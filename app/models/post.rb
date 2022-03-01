90da7b44e66227328b7a20e7a8d4160132bd9c43
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-28 23:54:07 -0500

    commit tree 57682008ea587fa092840844981b106b10eea9f0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_is_clickbait?
+
+    private
+
+    def title_is_clickbait?
+        titles = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+        if titles.none? { |t| t.match title } 
+            errors.add(:title, "title is clickbait")
+        end
+    end
+
 end
